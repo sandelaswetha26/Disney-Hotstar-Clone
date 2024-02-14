fa177897b526461526f03b371d4901d4caf9a87e
swethavarma26 <sandelaswetha57@gmail.com>
2024-02-14 10:42:31 +0530

    Disney

---
+++ README.md
@@ -0,0 +1,36 @@
+# -Disney-Hotstar-Clone
+ Disney+Hotstar-inspired web application! This project, crafted using HTML, CSS, and JavaScript, aims to replicate the immersive streaming experience of a popular OTT platform.
+
+ #  User Authentication:
+Secure user registration and login system to personalize the user experience.
+
+# Content Catalog:
+A rich catalog of movies, TV shows, and live sports, categorized for easy navigation.
+
+# Video Streaming:
+Seamless video playback with support for different resolutions.
+
+# Search Functionality:
+Users can search for specific content based on titles, genres, or keywords.
+
+# Watchlist:
+Users can create and manage a personal watchlist for bookmarking content to watch later
+
+# Latest Releases:
+Highlighting recently added movies, TV shows, and sports events.
+
+# Technologies Used
+
+This project was developed using a combination of front-end technologies to create an engaging and responsive streaming experience:
+
+ # HTML (Hypertext Markup Language):
+  - Used for structuring the content and layout of the web pages.
+
+ # CSS (Cascading Style Sheets):
+  - Responsible for styling and theming, ensuring an attractive and visually consistent user interface.
+
+ # JavaScript:
+- Implemented to enhance interactivity and user experience, including features like seamless video playback, search functionality, and dynamic content presentation.
+
+
+
---
+++ data.js
@@ -0,0 +1,32 @@
+let movies=[
+    {
+        name:"Baahubali 2: The Conclusion",
+        des:"Why did Kattappa Kill Amerendra Baahubali? Unvell the mystery and many more secrets that lie in the depths of the Mahishmati kingdom.",
+        image:"images/s1.jpg"
+    },
+    {
+        name:"K.G.F: Chapter 2",
+        des:"Rocky successfully rises as the leader and saviour of the people of the Kolar Gold Fields. But, in his goal to fulfil his mother's wishes, Rocky must tackle Adheera, Inayat Khalil and Ramika Sen.",
+        image:"images/s2.jpg"
+    },
+    {
+        name:"Avengers: Endgame",
+        des:"After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again to reinvigorate their trounced allies and restore balance.",
+        image:"images/s3.jpg"
+    },
+    {
+        name:"Bigg Boss",
+        des:"Contestants from all walks of life are locked in a common house. They compete against each other to win a cash prize by saving themselves from eliminations based on public votes.",
+        image:"images/s4.webp"
+    },
+    {
+        name:"Dunki",
+        des:"A group of friends use a backdoor process to move to another country, then struggle to return home.",
+        image:"images/s5.jpg"
+    },
+    {
+        name:"Guppedantha Manasu",
+        des:"Life brings headstrong Vasudhara to clash with Rishi, an arrogant professor. What's in store for this pair of chalk and cheese?",
+        image:"images/s6.webp"
+    }
+]
